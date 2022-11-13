7f8055d6dfd83122a60b74aeda0a06244670ed74
Valerii Stefanyk <valeriistefanyk@gmail.com>
2022-11-13 15:33:04 +0200

    Add views to create/edti blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseForbidden
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     context = {
         'posts': posts,
     }
@@ -12,7 +14,46 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    can_edit = request.user.is_authenticated and request.user == post.author
     context = {
-        'post': post
+        'post': post,
+        'can_edit': can_edit,
     }
     return render(request, 'blog/post_detail.html', context)
+
+def post_new(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post: Post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'blog/post_edit.html', context)
+    return HttpResponseForbidden()
+
+def post_edit(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk, author=request.user)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        context = {
+            'form': form,
+        }
+        return render(request, 'blog/post_edit.html', context)
+    return HttpResponseForbidden()
